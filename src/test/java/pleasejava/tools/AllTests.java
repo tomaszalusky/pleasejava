576aa73a0b401207632736a62b7d12f3914fa7ec
Tomas Zalusky <tomas.zalusky@intelis.cz>
2014-08-25 22:57:05 +0200

    + suite AllTests

---
+++ src/test/java/pleasejava/tools/AllTests.java
@@ -0,0 +1,20 @@
+package pleasejava.tools;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * All tests. Subsequent test cases do a little bit more testing than previous ones,
+ * in case of failure it's better to start fixing former tests.
+ * (This is not a case of intertest dependency, only hint for better test maintenance.)
+ * @author Tomas Zalusky
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+		TypeGraphLoadTest.class,
+		TypeGraphTopologicalOrderingTest.class
+})
+public class AllTests {
+
+}
