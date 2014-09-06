2ca84543a4089cc08268bcd8140ed5058a0ba9c0
Tomas Zalusky <tomas.zalusky@intelis.cz>
2014-09-06 23:42:59 +0200

    minor change

--- src/test/java/pleasejava/tools/TypeNodeTreeTest.java
+++ src/test/java/pleasejava/tools/TypeNodeTreeTest.java
@@ -43,7 +43,7 @@ public class TypeNodeTreeTest extends AbstractTypeGraphTest {
 		List<Type> topologicalOrdering = graph.getTopologicalOrdering();
 		TypeNode rootNode = topologicalOrdering.get(0).toTypeNode(null,0);
 		String actual = rootNode.toString();
-		//System.out.println(rootNode.toTransferObject().toString());
+		System.out.println(rootNode.toTransferObject().toString());
 		if (record) {
 			writeExpectedOutput(getClass(),name,actual);
 		} else {
