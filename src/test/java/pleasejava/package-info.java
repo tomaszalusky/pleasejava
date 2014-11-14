9391e5556e27caa80fb89dc80f223389066a5019
Tomas Zalusky <tomas.zalusky@intelis.cz>
2014-11-14 06:35:47 +0100

    + package-info.java + schema of dependencies of test concepts

---
+++ src/test/java/pleasejava/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * - nacist ibt pres JDBC
+ * - nacist derave nst pres JDBC
+ * - zrevidovat tvorbu TO u jdbct kolekci
+ * - zapracovat koncept JDBC-transferrable typu, a la ifc JdbcTransfer { convertForth(); convertBack(); } - to bude az u TO, mozna i dale
+ * - overit funkcnost pro dosavadni testcasy
+ * - overit funkcnost pro nove jdbct typy
+ * 
+ * later:
+ * - komentar patri az k TO: potomka typu trojstavový boolean, který charakterizuje nastavení null u prvkù (true/false), a pøíznak smazání prvku (null) POZN. ZREVIDOVAT, NEZDA SE MI, ZE BY V JEDNOM MISTE BYLA INFORMACE O CELE TABULCE I O JEJICH PRVCICH, A ZE NEMA TOTEZ VARRAY
+ * - vyresit algoritmus plneni TOT z PIT pro jdbct kolekce v  kolekci
+ * - vyresit globalni PLSQLConfig, inspirovat se v JOOQ
+ * 
+ */
+package pleasejava;
