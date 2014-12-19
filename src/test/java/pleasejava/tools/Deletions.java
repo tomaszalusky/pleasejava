82b70b8efc2764264aac1d96b7d4fd19e48c2d85
Tomas Zalusky <tomas.zalusky@intelis.cz>
2014-12-20 00:31:11 +0100

    * semantics od Deletions TO

--- src/test/java/pleasejava/tools/Deletions.java
+++ src/test/java/pleasejava/tools/Deletions.java
@@ -5,14 +5,14 @@ import java.util.List;
 import com.google.common.collect.Lists;
 
 /**
- * Collection of flags representing holes in nested table.
+ * Collection of deleted indices (holes) in nested table.
  * This specific {@link TransferObject} is utilized only for nested tables
- * and only for those of them which don't have continous sequence of indexes due to deletions.
+ * and only for those of them which don't have continuous sequence of indexes due to deletions.
  * @author Tomas Zalusky
  */
 public class Deletions extends TransferObject {
 
-	private List<Boolean> data = Lists.newArrayList();
+	private List<Integer> data = Lists.newArrayList();
 
 	public Deletions(TransferObject parent, TypeNode typeNode) {
 		super(parent, typeNode, typeNode.id() + "d");
